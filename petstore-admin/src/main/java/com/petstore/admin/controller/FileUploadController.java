66d92430347f22f8967ec7ae15a81cfe40fec4c3
aigistnalian <aigistnalian@gmail.com>
2015-07-15 11:11:31 +0530

    another change

--- petstore-admin/src/main/java/com/petstore/admin/controller/FileUploadController.java
+++ petstore-admin/src/main/java/com/petstore/admin/controller/FileUploadController.java
@@ -2,28 +2,54 @@ package com.petstore.admin.controller;
 
 import java.io.Serializable;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ManagedProperty;
 
 import org.primefaces.event.FileUploadEvent;
 
-@ManagedBean
-@RequestScoped
+import com.petstore.admin.bean.ProductItem;
+
 public class FileUploadController implements Serializable
 {
+	@ManagedProperty(value="#{item}")
+	ProductItem productItem;
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private String item;
 	/**
 	 * @param event
 	 */
 	public void uploadFile(FileUploadEvent event) 
 	{
         byte[] file = event.getFile().getContents();
-
+        System.out.println(file);
+        System.out.println(getItem());
         System.out.println("MADE IT INTO FILE UPLOAD !!! ");
    }
+	
+	public void setProductItem(ProductItem productItem) {
+		this.productItem = productItem;
+	}
+	
+	public ProductItem getProductItem() {
+		return productItem;
+	}
+
+	/**
+	 * @return the item
+	 */
+	public String getItem() {
+		return item;
+	}
+
+	/**
+	 * @param item the item to set
+	 */
+	public void setItem(String item) {
+		this.item = item;
+	}
+
 }
